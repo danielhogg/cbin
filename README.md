b7e4329028e3dc81b194ad861341456bf7553a0e
Daniel Hogg <daniel.hogg@mcgill.ca>
2019-01-11 14:06:47 -0500

    workshop code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cbin
 This repository is for an upcoming workshop on calling C binaries from Python scripts.
+
+Two binaries, 'generate_sample' and 'analyze_sample'
+
---
+++ process_sample.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	printf("Error in process_sample: Process expects exactly 1 command-line argument.");
+	printf("Given: 0");
+
+	printf("Error in process_sample: Did not find file at the following path:\n");
+
+}
